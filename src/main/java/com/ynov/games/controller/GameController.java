f3895c54ab9b2f4b2725af5c21445430cf98590e
Delphine <delphine.pva@gmail.com>
2024-03-11 00:11:55 +0100

    version one added with RequestMapping

--- src/main/java/com/ynov/games/controller/GameController.java
+++ src/main/java/com/ynov/games/controller/GameController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.PostMapping;
 //import org.springframework.web.bind.annotation.PutMapping;
 //import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +18,7 @@ import com.ynov.games.model.Game;
 import com.ynov.games.service.GameService;
 
 @RestController
+@RequestMapping("/api/v1")
 public class GameController {
 	
 	@Autowired
--- src/main/java/com/ynov/games/controller/ServerController.java
+++ src/main/java/com/ynov/games/controller/ServerController.java
@@ -1,9 +1,11 @@
 package com.ynov.games.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1")
 public class ServerController {
 	
 	@GetMapping("/")
