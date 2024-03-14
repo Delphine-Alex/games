d5cd02c171432fb92e467f5194be4a053f22784d
Delphine <delphine.pva@gmail.com>
2024-03-14 16:37:03 +0100

    tournaments endpoint done

--- src/main/java/com/ynov/games/model/Tournament.java
+++ src/main/java/com/ynov/games/model/Tournament.java
@@ -1,10 +1,17 @@
 package com.ynov.games.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -22,8 +29,26 @@ public class Tournament {
 	@Column(name = "id_game")
 	private Integer id_game;
 	
+	@OneToMany(
+			cascade = CascadeType.ALL,
+			fetch = FetchType.EAGER,
+			orphanRemoval = true
+			)
+	@JoinColumn(name = "id_game")
+	
+	private List<Game> games = new ArrayList<>();
+	
 	@Column(name = "id_langage")
 	private Integer id_langage;
+	
+	@OneToMany(
+			cascade = CascadeType.ALL,
+			fetch = FetchType.EAGER,
+			orphanRemoval = true
+			)
+	@JoinColumn(name = "id_langage")
+	
+	private List<Langage> langages = new ArrayList<>();
 	
 	
 	// Getters and setters
@@ -53,6 +78,14 @@ public class Tournament {
 		this.id_game = id_game;
 	}
 
+	public List<Game> getGames() {
+		return games;
+	}
+
+	public void setGames(List<Game> games) {
+		this.games = games;
+	}
+
 	public Integer getId_langage() {
 		return id_langage;
 	}
@@ -60,6 +93,16 @@ public class Tournament {
 	public void setId_langage(Integer id_langage) {
 		this.id_langage = id_langage;
 	}
+
+	public List<Langage> getLangages() {
+		return langages;
+	}
+
+	public void setLangages(List<Langage> langages) {
+		this.langages = langages;
+	}
+	
+	
 	
 	
 
