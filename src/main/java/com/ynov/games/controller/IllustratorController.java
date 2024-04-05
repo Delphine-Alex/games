ec5491e096cbf6d73a831d3e8f22df4fc32d795a
Delphine <delphine.pva@gmail.com>
2024-04-05 12:01:28 +0200

    pagination added on illustrator endpoint

--- src/main/java/com/ynov/games/controller/IllustratorController.java
+++ src/main/java/com/ynov/games/controller/IllustratorController.java
@@ -3,6 +3,9 @@ package com.ynov.games.controller;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ynov.games.model.Illustrator;
@@ -25,11 +29,20 @@ public class IllustratorController {
 	private IllustratorService illustratorService;
 	
 	@GetMapping("/illustrators")
-	public ResponseEntity<Iterable<Illustrator>> getIllustrators() {
-	    Iterable<Illustrator> illustrators = illustratorService.getIllustrators();
-	    
-	    return ResponseEntity.status(HttpStatus.OK).body(illustrators);
-	}
+	public ResponseEntity<Page<Illustrator>> getillustrators(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int pageSize,
+            @RequestParam(required = false) String name
+    ) {
+        Pageable pageable = PageRequest.of(page, pageSize);
+        Page<Illustrator> illustratorsPage = illustratorService.getIllustrators(pageable, name);
+        
+        if (illustratorsPage.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(illustratorsPage, HttpStatus.OK);
+        }
+    }
 	
 	@GetMapping("/illustrator/{id}")
 	public ResponseEntity<Illustrator> getIllustrator(@PathVariable("id") Integer id) {
--- src/main/java/com/ynov/games/service/IllustratorService.java
+++ src/main/java/com/ynov/games/service/IllustratorService.java
@@ -3,6 +3,8 @@ package com.ynov.games.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.ynov.games.model.Illustrator;
@@ -14,9 +16,13 @@ public class IllustratorService {
 	@Autowired
 	private IllustratorRepository illustratorRepository;
 	
-	public Iterable<Illustrator> getIllustrators(){
-		return illustratorRepository.findAll();
-	}
+	public Page<Illustrator> getIllustrators(Pageable pageable, String name) {
+        if (name != null) {
+            return illustratorRepository.findAllByName(pageable, name);
+        } else {
+            return illustratorRepository.findAll(pageable);
+        }
+    }
 	
 	public Optional<Illustrator> getIllustrator(Integer id) {
 		return illustratorRepository.findById(id);
